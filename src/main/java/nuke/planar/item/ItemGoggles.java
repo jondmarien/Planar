ec0c45256e2f49a932a900752a86dcf2202c78be
Jon Marien <nuclearsmilz@gmail.com>
2017-05-19 21:08:42 -0400

    Added goggles texture + item

---
+++ src/main/java/nuke/planar/item/ItemGoggles.java
@@ -0,0 +1,11 @@
+package nuke.planar.item;
+
+import nuke.planar.item.base.*;
+
+public class ItemGoggles extends PlanarItemBase {
+
+	public ItemGoggles() {
+		super("goggles", true);
+	}
+	
+}
--- src/main/java/nuke/planar/reg/PlanarContent.java
+++ src/main/java/nuke/planar/reg/PlanarContent.java
@@ -6,18 +6,19 @@ import net.minecraft.block.*;
 import net.minecraft.block.material.*;
 import net.minecraft.item.*;
 import nuke.planar.block.base.*;
+import nuke.planar.item.*;
 import nuke.planar.item.base.*;
 
 public class PlanarContent {
 	public static ArrayList<Item> items = new ArrayList<Item>();
 	public static ArrayList<Block> blocks = new ArrayList<Block>();
 	
-	public static Item item_test;
+	public static Item goggles;
 
 	public static Block block_test;
 
 	public static void init() {
-		items.add(item_test = new PlanarItemBase("item_test", true));
+		items.add(goggles = new ItemGoggles());
 
 		blocks.add(block_test = (new PlanarBlockBase(Material.ROCK, "block_test", "pickaxe", 1, 1.0F, 11.0F,
 		        SoundType.STONE, true).setIsFullCube(true).setIsOpaqueCube(true).setLightOpacity(16)));
